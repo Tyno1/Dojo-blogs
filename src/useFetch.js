d27c65aca4ff909749fbd40a3891234ba2c1f01c
tyno designz <tynodesignz@gmail.com>
2023-03-09 17:09:42 -0500

    first commit

---
+++ src/useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+        useEffect(() => {
+          setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if (!res.ok){
+                        throw Error('Could not fetch data for that resource')
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setData(data);
+                    setIsLoading(false);
+                    setError(null)
+                })
+                .catch(err => {
+                    setIsLoading(false)
+                    setError(err.message); 
+                })
+        
+          }, 2000)  
+
+        }, [url])
+
+    return {data, isLoading, error};
+
+}
+
+
+export default useFetch;
